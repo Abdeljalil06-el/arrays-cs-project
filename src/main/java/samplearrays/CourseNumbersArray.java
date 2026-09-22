e478cf397710b2320f618fc919e9164dd2bcb4db
Abdeljalil06-el <abdeljalil.elachehab@um6p.ma>
2026-09-22 22:21:53 +0000

    end the exercice 1: CourseNumbersArray

--- src/main/java/samplearrays/CourseNumbersArray.java
+++ src/main/java/samplearrays/CourseNumbersArray.java
@@ -3,6 +3,29 @@ package samplearrays;
 public class CourseNumbersArray {
     public static void main(String[] args) {
         int[] registeredCourses = {1010, 1020, 2080, 2140, 2150, 2160};
+        int newCourse=2000;
+        int[] upatedCourses=new int[registeredCourses.length+1];
 
+        for (int i=0;i<registeredCourses.length;i++) {
+            upatedCourses[i]=registeredCourses[i];
+        }
+        upatedCourses[registeredCourses.length]=newCourse;
+        
+        System.out.println("the updated courses are : ");
+        for (int i=0;i<upatedCourses.length;i++) {
+                System.out.println(upatedCourses[i]);
+        }
+
+        int scourse=3000;
+        boolean flag=false;
+        for (int course : upatedCourses) {
+            if (course==scourse){
+                flag=true;
+            break;
+            }
+        }
+        if (flag) System.out.println("the course was found.");
+        else System.out.println("the course was not found.");
+            
     }
 }
